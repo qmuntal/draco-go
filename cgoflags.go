dd150a539f9d2ffd78ad0c829e85f4c44720b50a
Quim Muntal <quimmuntal@gmail.com>
2021-02-08 20:50:03 +0100

    add lstdc++

--- cgoflags.go
+++ cgoflags.go
@@ -6,6 +6,6 @@ package draco
 #cgo windows CFLAGS: -DDRACO_STATIC
 #cgo windows,amd64 LDFLAGS: -lcdraco_windows_amd64 -lstdc++
 #cgo linux,amd64 LDFLAGS: -lcdraco_linux_amd64 -lstdc++ -lm
-#cgo darwin,amd64 LDFLAGS: -lcdraco_darwin_amd64
+#cgo darwin,amd64 LDFLAGS: -lcdraco_darwin_amd64 -lstdc++ -lm
 */
 import "C"
