f402c790c72506651b9f78e4a1cce11c49dc5c45
Quim Muntal <quimmuntal@gmail.com>
2021-02-08 20:44:25 +0100

    darwin fix

--- cgoflags.go
+++ cgoflags.go
@@ -2,11 +2,10 @@
 package draco
 
 /*
-#cgo CXXFLAGS: -std=c++11
 #cgo LDFLAGS: -L${SRCDIR}/lib
 #cgo windows CFLAGS: -DDRACO_STATIC
 #cgo windows,amd64 LDFLAGS: -lcdraco_windows_amd64 -lstdc++
 #cgo linux,amd64 LDFLAGS: -lcdraco_linux_amd64 -lstdc++ -lm
-#cgo darwin,amd64 LDFLAGS: -lcdraco_darwin_amd64 -lstdc++ -lm
+#cgo darwin,amd64 LDFLAGS: -lcdraco_darwin_amd64
 */
 import "C"
